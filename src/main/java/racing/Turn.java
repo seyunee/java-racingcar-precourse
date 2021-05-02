831d49ca3531e1be97a38f5c0ca5934566c320e8
seyun <seyun26@gmail.com>
2021-05-02 17:20:07 +0900

    feat : 1회 시도 당 실행결과 프린트

--- src/main/java/racing/Turn.java
+++ src/main/java/racing/Turn.java
@@ -1,13 +1,26 @@
 package racing;
 
+import java.util.List;
+
 public class Turn {
     public int getTurnNo() {
         return (int) (Math.random() * 9);
     }
 
-    public void turnResult(Car car,int turnNo) {
-        if(car.turn(turnNo).equals(CarStatus.MOVE)){
-            car.setLocation(car.getLocation()+1);
+    public void turnResult(Car car, int turnNo) {
+        if (car.turn(turnNo).equals(CarStatus.MOVE)) {
+            car.setLocation(car.getLocation() + 1);
         }
+    }
+
+    public void turn(List<Car> cars) {
+        for (Car car : cars) {
+            turnResult(car, getTurnNo());
+            System.out.print("\n" + car.getName() + " : ");
+            for (int i = 0; i < car.getLocation(); i++) {
+                System.out.print("-");
+            }
+        }
+        System.out.print("\n");
     }
 }
