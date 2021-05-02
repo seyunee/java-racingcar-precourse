cddefb25b974a2b7845165033455d6b713f11923
seyun <seyun26@gmail.com>
2021-05-02 17:21:12 +0900

    refactor : 사용자입력, exception처리 추가

--- src/main/java/racing/Racing.java
+++ src/main/java/racing/Racing.java
@@ -8,31 +8,31 @@ public class Racing {
     static final String delimiter = ",";
     Scanner scanner = new Scanner(System.in);
 
+    public void start() {
+        List<Car> cars = getCarNames();
+        int turnCount = 0, tryCount = 0;
+
+        tryCount = getTryCount();
+
+        System.out.print("\n실행 결과");
+        while (turnCount < tryCount) {
+            turnCount++;
+            Turn turn = new Turn();
+            turn.turn(cars);
+        }
+
+        System.out.print("\n" + printWinners(getWinner(cars)) + "가 최종 우승했습니다.");
+        scanner.close();
+    }
+
     public static List<Car> participate(String carNames) {
         List<Car> cars = new ArrayList<>();
-        if(ValidationUtil.duplicateCars(carNames,ValidationUtil.splitCars(carNames))){
+        if (ValidationUtil.duplicateCars(carNames, ValidationUtil.splitCars(carNames))) {
             for (String carName : carNames.split(delimiter)) {
                 cars.add(new Car(carName));
             }
         }
         return cars;
-    }
-
-    public void start() {
-        int tryCount = 0;
-        System.out.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String carNames = scanner.nextLine();
-        List<Car> cars = participate(carNames);
-
-        System.out.print("시도할 회수는 몇회인가요?");
-        String userTryCount = scanner.nextLine();
-        while (tryCount < ValidationUtil.tryCount(userTryCount)) {
-            tryCount++;
-            // todo : 이동시도
-        }
-
-        System.out.print(printWinners(getWinner(cars)) + "가 최종 우승했습니다.");
-        scanner.close();
     }
 
     public List<Car> getWinner(List<Car> cars) {
@@ -56,5 +56,28 @@ public class Racing {
             }
         }
         return winner;
+    }
+
+    public List<Car> getCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNames = scanner.nextLine();
+
+        try {
+            return participate(carNames);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return getCarNames();
+        }
+    }
+
+    public int getTryCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        String userTryCount = scanner.nextLine();
+        try {
+            return ValidationUtil.tryCount(userTryCount);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return getTryCount();
+        }
     }
 }
